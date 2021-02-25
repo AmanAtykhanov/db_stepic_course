a57de7e2dcd0c4eca5f5e78ab72f5324868adec7
AmanAtykhanov <aman.atykhanov@mail.ru>
2021-02-25 13:12:48 +0600

    Delete exercise 2.3 inner join 4 tables.sql

--- exercise 2.3 inner join 4 tables.sql
+++
@@ -1,8 +0,0 @@
-select client.first_name, client.last_name, count(sale.status_id) as new_sale_num from sale
-inner join client
-on client.id = sale.client_id
-inner join status
-on status.id = sale.status_id
-inner join sale_history
-on sale_history.sale_id = sale.id
-group by client.first_name, client.last_name;
