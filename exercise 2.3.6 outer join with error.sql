cfba6ed86b2788a38eeb7d3199006e2c01499fc0
AmanAtykhanov <aman.atykhanov@mail.ru>
2021-02-26 16:32:27 +0600

    Delete exercise 2.3.6 outer join with error.sql

--- exercise 2.3.6 outer join with error.sql
+++
@@ -1,5 +0,0 @@
-select good.name as good_name, category.name as category_name from category_has_good as chg
-	right outer join category as c 
-    on chg.category_id = c.id
-    right outer join good as g
-    on chg.good_id = g.id;
