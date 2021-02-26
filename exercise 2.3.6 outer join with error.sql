a19ae486a001c78746455ff7b5cdbd802a45efdb
AmanAtykhanov <aman.atykhanov@mail.ru>
2021-02-26 14:42:04 +0600

    Update exercise 2.3.6 outer join with error.sql

--- exercise 2.3.6 outer join with error.sql
+++ exercise 2.3.6 outer join with error.sql
@@ -1,4 +1,4 @@
-select category.id from category_has_good as chg
+select good.name as good_name, category.name as category_name from category_has_good as chg
 	right outer join category as c 
     on chg.category_id = c.id
     right outer join good as g
