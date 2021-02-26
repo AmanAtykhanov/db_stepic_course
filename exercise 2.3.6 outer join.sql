ae9ead6df1a338d64493ace36998fe1f9c599c5e
AmanAtykhanov <aman.atykhanov@mail.ru>
2021-02-26 16:33:37 +0600

    Update exercise 2.3.6 outer join.sql

--- exercise 2.3.6 outer join.sql
+++ exercise 2.3.6 outer join.sql
@@ -1,4 +1,4 @@
-select *from category_has_good as chg
+select g.name as good_name, c.name as category_name from category_has_good as chg
 	right outer join category as c 
     on chg.category_id = c.id
     right outer join good as g
